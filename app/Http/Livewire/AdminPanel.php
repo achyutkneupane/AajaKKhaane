7d6754dcc226c6f305a1120536673a1be58fe5fe
Achyut Neupane <achyutkneupane@gmail.com>
2022-12-30 23:07:17 +0545

    Voting permission toggler added

--- app/Http/Livewire/AdminPanel.php
+++ app/Http/Livewire/AdminPanel.php
@@ -6,8 +6,19 @@ use Livewire\Component;
 
 class AdminPanel extends Component
 {
+    public $users;
+    public function togglePermission($user, $permission)
+    {
+        $user = \App\Models\User::find($user);
+        if ($user->hasPermissionTo($permission)) {
+            $user->revokePermissionTo($permission);
+        } else {
+            $user->givePermissionTo($permission);
+        }
+    }
     public function render()
     {
+        $this->users = \App\Models\User::get();
         return view('livewire.admin-panel');
     }
 }
